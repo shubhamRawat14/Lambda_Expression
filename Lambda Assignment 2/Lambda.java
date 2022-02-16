43da2d560aab350c0c3c06ac7dddbebb86924713
shubhamRawat14 <94029087+shubhamRawat14@users.noreply.github.com>
2022-02-16 23:33:14 +0530

    Create Lambda.java

---
+++ Lambda Assignment 2/Lambda.java
@@ -0,0 +1,13 @@
+interface EvenNumber{
+    long NextEvenNumber(long Number1);
+}
+public class Lambda {
+    public static void main(String[] args) {
+    
+        EvenNumber object1= (num)-> num+2 -(num%2);
+        
+        System.out.println("The Next Even number is : "+object1.NextEvenNumber(1123));
+        
+    }
+    
+}
